2121d3e374bf56711f6f09bc76243a0dc276ff25
草苺奶昔LMT <45849137+981377660LMT@users.noreply.github.com>
2020-10-04 22:52:59 +0800

    Update pipelines.py

--- Arknights/Arknights/pipelines.py
+++ Arknights/Arknights/pipelines.py
@@ -1,14 +1,58 @@
 import json, codecs, os, csv
 from itemadapter import ItemAdapter
 import uuid
+import pymysql
 
-id = uuid.uuid4() #生成唯一标识
+class ArkSpiderPipeline(object):
+    def __init__(self):
+        # 1. 建立数据库的连接
+        self.connect = pymysql.connect(
+	    # 连接的是本地数据库
+            host='192.168.5.128',
+            # mysql数据库的端口号
+            port=3306,
+            # 数据库的用户名
+            user='root',
+            # 本地数据库密码
+            passwd='a2818088',
+            # 数据库名
+            db='FGO',
+            # 编码格式
+            charset='utf8'
+        )
+        # 2. 创建一个游标cursor, 是用来操作表。
+        self.cursor = self.connect.cursor()
+
+    def process_item(self, item, spider):
+        # 3. 将Item数据放入数据库，默认是同步写入。
+        insert_sql = "INSERT INTO FGO(中文文本,日文文本) VALUES ('%s', '%s')" % (item['中文文本'], item['日文文本']) #FGO是表名
+        self.cursor.execute(insert_sql)
+
+        # 4. 提交操作
+        self.connect.commit()
+
+    def close_spider(self, spider):
+        self.cursor.close()
+        self.connect.close()
+
+
+
+
+
+
+
+
+
+
+
+
+'''id = uuid.uuid4() #生成唯一标识
 
 # 保存为csv文件
 class Pipiline_ToCSV(object):
     def __init__(self):
         #文件的位置
-        store_file = os.path.dirname(__file__) + '/spiders/output_{}.csv'.format(id) #__file__表示显示文件当前的位置Arknights文件夹
+        store_file = os.path.dirname(__file__) + '/spiders/outputFGO_{}.csv'.format(id) #__file__表示显示文件当前的位置Arknights文件夹
         #打开文件，并设置编码
         self.file = codecs.open(filename= store_file, mode= 'wb', encoding='utf-8-sig') #这里要看一下是utf-8还是utf-8-sig，不对则会乱码
 
@@ -22,7 +66,7 @@ class Pipiline_ToCSV(object):
         return item
 
     def close_spider(self, spider):
-        self.file.close()
+        self.file.close()'''
         
 # # 保存为json文件
 # class JsonPipeline(object):
